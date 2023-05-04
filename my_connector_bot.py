d5f446b94f6dab7c7e9424921e17b56e0d0b51e2
Yuriy Baragin <yubarmalei@gmail.com>
2023-05-04 20:17:22 +0300

    bot7

--- my_connector_bot.py
+++ my_connector_bot.py
@@ -1,17 +1,24 @@
 import mysql.connector
 
 
-def run_query(query, host, user, password, database):
-    mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
+class QueryError(Exception):
+    pass
 
-    mycursor = mydb.cursor()
-    mycursor.execute(query)
-    result = mycursor.fetchall()
-    mydb.commit()
-    mydb.close()
-    return result
+
+def run_query(query, host, user, password, database):
+    try:
+        mydb = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+
+        mycursor = mydb.cursor()
+        mycursor.execute(query)
+        result = mycursor.fetchall()
+        mydb.commit()
+        mydb.close()
+        return result
+    except mysql.connector.Error as error:
+        raise QueryError(str(error))
